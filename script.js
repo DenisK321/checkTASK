6a62b69e33793e4c74d716f459764756087bcb4a
Denis <komnnatnyy2@gmail.com>
2019-03-09 12:14:43 +0200

    first commit

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ let logins = ['Sasha', 'Dmitry', 'Ilya'];
 
 let whatIsYourLogin = prompt('What Is your login');
 
-const isLoginValid = (obj)  => !!Oblect.keys(obj).lendth >= 4 && Oblect.keys(obj).lendth <= 16;
+const isLoginValid = (str) => str.length >= 4 && str.length <= 16;
+
+const isLoginUnique = (masiv, login) => masiv.indexOf(login) === -1;
 
 console.log(
     isLoginValid(whatIsYourLogin)
 );
-
-
-// const isLoginValid = function(whatIsYourLogin){
-//     whatIsYourLogin = whatIsYourLogin.length >= 4 && whatIsYourLogin.length <= 16
-//     return whatIsYourLogin
-// }
-
+console.log(
+    isLoginUnique(logins, whatIsYourLogin)
+)
